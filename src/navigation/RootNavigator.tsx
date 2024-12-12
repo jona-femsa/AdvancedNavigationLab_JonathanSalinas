1b03a077046594101f697b4436c1332cb4ae1e8d
Jonathan Salinas <jonathan.salinas@digitalfemsa.com>
2024-12-12 06:01:02 -0600

    deeplinks

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,15 +1,33 @@
-import { NavigationContainer } from '@react-navigation/native'
+import { LinkingOptions, NavigationContainer } from '@react-navigation/native'
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from '../store';
 import AppDrawerNavigator from './AppDrawerNavigator';
 import AuthStackNavigator from './AuthStackNavigator';
 
+const linking: LinkingOptions = {
+  prefixes: ["labapp://", "https://labapp.local"],
+  config: {
+    screens: {
+      Home: "home",
+      Settings: "settings",
+      Profile: "profile",
+      Feed: {
+        path: "feed",
+        screens: {
+          Details: "details/:id",
+        },
+      },
+    },
+  },
+};
+
+
 const RootNavigator = () => {
   const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
 
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={ linking }>
         { isLoggedIn ? <AppDrawerNavigator/> : <AuthStackNavigator /> }
     </NavigationContainer>
   )
